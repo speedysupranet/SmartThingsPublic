8e7740515fcdcdaae588e1159ec41924348b4bbd
Werner Stocker <werner.stocker@libersec.li>
2018-11-11 21:20:27 +0100

    MyApps

--- smartapps/jasonbsteele/grovestreams-quickstart.src/grovestreams-quickstart.groovy
+++ smartapps/jasonbsteele/grovestreams-quickstart.src/grovestreams-quickstart.groovy
@@ -14,65 +14,65 @@
  * for the specific language governing permissions and limitations under the License.
  *
  */
- 
+
 definition(
-                name: "GroveStreams QuickStart",
-                namespace: "JasonBSteele",
-                author: "Jason Steele",
-                description: "Log to GroveStreams",
-                category: "My Apps",
-                iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-                iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-                iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
- 
+		name: "GroveStreams QuickStart",
+		namespace: "JasonBSteele",
+		author: "Jason Steele",
+		description: "Log to GroveStreams",
+		category: "My Apps",
+		iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+		iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+		iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
 preferences {
-        section("Log devices...") {
-                input "contacts", "capability.contactSensor", title: "Doors open/close", required: false, multiple: true
-                input "temperatures", "capability.temperatureMeasurement", title: "Temperatures", required:false, multiple: true
-        }
- 
-        section ("GroveStreams Feed PUT API key...") {
-                input "channelKey", "text", title: "API key"
-        }
+	section("Log devices...") {
+		input "contacts", "capability.contactSensor", title: "Doors open/close", required: false, multiple: true
+		input "temperatures", "capability.temperatureMeasurement", title: "Temperatures", required:false, multiple: true
+	}
+
+	section ("GroveStreams Feed PUT API key...") {
+		input "channelKey", "text", title: "API key"
+	}
 }
- 
+
 def installed() {
-        initialize()
+	initialize()
 }
- 
+
 def updated() {
-        unsubscribe()
-        initialize()
+	unsubscribe()
+	initialize()
 }
- 
+
 def initialize() {
-        subscribe(temperatures, "temperature", handleTemperatureEvent)
-        subscribe(contacts, "contact", handleContactEvent)
+	subscribe(temperatures, "temperature", handleTemperatureEvent)
+	subscribe(contacts, "contact", handleContactEvent)
 }
- 
+
 def handleTemperatureEvent(evt) {
-        sendValue(evt) { it.toString() }
+	sendValue(evt) { it.toString() }
 }
- 
+
 def handleContactEvent(evt) {
-        sendValue(evt) { it == "open" ? "true" : "false" }
+	sendValue(evt) { it == "open" ? "true" : "false" }
 }
- 
+
 private sendValue(evt, Closure convert) {
-        def compId = URLEncoder.encode(evt.displayName.trim())
-        def streamId = evt.name
-        def value = convert(evt.value)
-       
-        log.debug "Logging to GroveStreams ${compId}, ${streamId} = ${value}"
-       
-        def url = "https://grovestreams.com/api/feed?api_key=${channelKey}&compId=${compId}&${streamId}=${value}"
-        def putParams = [
-                uri: url,
-                body: []]
- 
-        httpPut(putParams) { response ->
-                if (response.status != 200 ) {
-                        log.debug "GroveStreams logging failed, status = ${response.status}"
-                }
-        }
+	def compId = URLEncoder.encode(evt.displayName.trim())
+	def streamId = evt.name
+	def value = convert(evt.value)
+	
+	log.debug "Logging to GroveStreams ${compId}, ${streamId} = ${value}"
+	
+	def url = "https://grovestreams.com/api/feed?api_key=${channelKey}&compId=${compId}&${streamId}=${value}"
+	def putParams = [
+		uri: url,
+		body: []]
+
+	httpPut(putParams) { response ->
+		if (response.status != 200 ) {
+			log.debug "GroveStreams logging failed, status = ${response.status}"
+		}
+	}
 }
